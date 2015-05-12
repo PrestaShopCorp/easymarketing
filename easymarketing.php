9c8ee6f075289f5666a4d77b80149be803687914
Thomas Nabord <thomas.nabord@prestashop.com>
2015-05-12 15:02:24 +0200

    Updated version (v0.4.0)

--- easymarketing.php
+++ easymarketing.php
@@ -35,7 +35,7 @@ class Easymarketing extends Module {
 	{
 		$this->name = 'easymarketing';
 		$this->tab = 'advertising_marketing';
-		$this->version = '0.3.0';
+		$this->version = '0.4.0';
 		$this->author = 'easymarketing';
 		$this->need_instance = 0;
 		$this->ps_versions_compliancy = array(
